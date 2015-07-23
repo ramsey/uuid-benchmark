0874dae3dc32c749d480fd6f07765980da93f235
Ben Ramsey <ben@benramsey.com>
2015-07-23 17:48:47 +0000

    Benchmarks for version 1 UUIDs.

---
+++ uuid1-benchmark.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$watch = new \Symfony\Component\Stopwatch\Stopwatch();
+
+define('ITERATIONS', 10000);
+
+$results = [];
+
+
+/**
+ * Using pecl-uuid by itself
+ */
+
+$watch->start('pecl');
+
+for ($i = 0; $i < ITERATIONS; ++$i) {
+    $x = uuid_create(UUID_TYPE_TIME);
+}
+
+$results['pecl'] = $watch->stop('pecl');
+
+
+/**
+ * Using the older Rhumsaa\Uuid version of the library
+ */
+
+if (class_exists('\Rhumsaa\Uuid\Uuid')) {
+    $watch->start('rhumsaa');
+
+    for ($i = 0; $i < ITERATIONS; ++$i) {
+        $x = (string) \Rhumsaa\Uuid\Uuid::uuid1();
+    }
+
+    $results['rhumsaa'] = $watch->stop('rhumsaa');
+}
+
+
+/**
+ * Using Ramsey\Uuid with the default time generator
+ */
+
+if (class_exists('\Ramsey\Uuid\Uuid')) {
+    $watch->start('ramsey-default-generator');
+
+    for ($i = 0; $i < ITERATIONS; ++$i) {
+        $x = (string) \Ramsey\Uuid\Uuid::uuid1();
+    }
+
+    $results['ramsey-default-generator'] = $watch->stop('ramsey-default-generator');
+}
+
+
+/**
+ * Using Ramsey\Uuid with a pecl-uuid time generator
+ */
+
+if (class_exists('\Ramsey\Uuid\Uuid')) {
+    $watch->start('ramsey-pecl-generator');
+
+    $uuidFactory = new \Ramsey\Uuid\UuidFactory();
+    $uuidFactory->setTimeGenerator(new \Ramsey\Uuid\Generator\PeclUuidTimeGenerator());
+    \Ramsey\Uuid\Uuid::setFactory($uuidFactory);
+
+    for ($i = 0; $i < ITERATIONS; ++$i) {
+        $x = (string) \Ramsey\Uuid\Uuid::uuid1();
+    }
+
+    $results['ramsey-pecl-generator'] = $watch->stop('ramsey-pecl-generator');
+}
+
+
+foreach ($results as $name => $result) {
+    printf('% 24s | %.04f sec/%d | %.07f sec/one' . PHP_EOL,
+        strtoupper($name),
+        $result->getDuration() / 1000,
+        ITERATIONS,
+        $result->getDuration() / 1000 / ITERATIONS);
+}
